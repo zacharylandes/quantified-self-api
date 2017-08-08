7a54f9fae0b8f42b56b942ad0329518be7d1d816
Casey Ann Cumbow <cumbow8@gmail.com>
2017-08-08 15:57:02 -0400

    Add model validations and tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "valid" do
+      it "with a name and calories" do
+        food = Food.new(name: "Blueberry Muffin", calories: 250)
+        expect(food).to be_valid
+      end
+    end
+
+    context "invalid" do
+      it "without a name" do
+        food = Food.new(calories: 250)
+        expect(food).to be_invalid
+      end
+
+      it "without calories" do
+        food = Food.new(name: "Blueberry Muffin")
+        expect(food).to be_invalid
+      end
+    end
+  end
 end
