4fbccde790fda2f05d6f2d5a7f2c27d7eccdb7af
Casey Ann Cumbow <cumbow8@gmail.com>
2017-08-08 14:55:10 -0400

    Create Food model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+foods = [
+          ["Banana", 150],
+          ["Bagel Bites - Four Cheese", 650],
+          ["Chicken Burrito", 800],
+          ["Grapes", 180],
+          ["Blueberry Muffins", 450],
+          ["Yogurt", 550],
+          ["Macaroni and Cheese", 950],
+          ["Granola Bar", 200],
+          ["Gum", 50],
+          ["Cheese", 400],
+          ["Fruit Snacks", 120],
+          ["Apple", 220]
+        ]
+
+foods.each do |food|
+  new_food = Food.create!(name: food.first, calories: food.last)
+  puts "Created #{new_food.name}"
+end
