34474cf8e3644ab3d788efc52cdad92d7832de0f
Kelley Jenkins <ckjenkins3@gmail.com>
2018-03-15 11:30:35 -0600

    Undoing what we did

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,5 @@ class Meal < ApplicationRecord
   validates :name, presence: true
 
   has_many :meal_foods
-  has_many :foods, through: :meal_foods, dependent: :destroy
+  has_many :foods, through: :meal_foods
 end
